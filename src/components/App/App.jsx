c54c80db5b890468c46f07ad4a979fbb91a5bd16
KsushaDk <81408959+KsushaDk@users.noreply.github.com>
2022-10-20 16:54:15 +0300

    add: animation for click

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,20 +8,35 @@ import { InfoPage } from 'Pages/InfoPage/InfoPage';
 import { Layout } from './Layout';
 import { Main } from '../Main/Main';
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Layout />}>
-			<Route index element={<LogInPage />} />
-			<Route path="home/*" element={<Main />} />
-			<Route path="signup" element={<SignUpPage />} />
-			<Route path="interview/:interviewId" element={<InterviewPage />} />
-			<Route path="info" element={<InfoPage />} />
-			<Route
-				path="*"
-				element={<ErrorPage link="/" message="This page doesn't exist." />}
-			/>
-		</Route>
-	</Routes>
-);
+const App = () => {
+	const clickEffect = (e) => {
+		const d = document.createElement('div');
+		d.className = 'clickEffect';
+		d.style.top = `${e.clientY}px`;
+		d.style.left = `${e.clientX}px`;
+		document.body.appendChild(d);
+		d.addEventListener('animationend', () => {
+			d.parentElement.removeChild(d);
+		});
+	};
+
+	document.addEventListener('click', clickEffect);
+
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<LogInPage />} />
+				<Route path="home/*" element={<Main />} />
+				<Route path="signup" element={<SignUpPage />} />
+				<Route path="interview/:interviewId" element={<InterviewPage />} />
+				<Route path="info" element={<InfoPage />} />
+				<Route
+					path="*"
+					element={<ErrorPage link="/" message="This page doesn't exist." />}
+				/>
+			</Route>
+		</Routes>
+	);
+};
 
 export default App;
