04e54b30796f229df52e481c67229b9b5410f9e9
Kseniya Disko <Kseniya.Disko@itechart-group.com>
2022-11-21 16:36:53 +0200

    fix: error in user table

--- src/utils/getQuantityForTableCell.js
+++ src/utils/getQuantityForTableCell.js
@@ -1,8 +1,11 @@
 import { getFromLSByKey } from 'Utils/funcForLSByKey';
 
 export const getQuantityForTableCell = (id, keyLS) => {
-	const dataFromLS = getFromLSByKey(keyLS).filter((item) =>
-		keyLS === 'answers' ? item.id === id : item.author === id
-	);
-	return dataFromLS.length;
+	const dataFromLS = getFromLSByKey(keyLS);
+
+	return dataFromLS
+		? dataFromLS.filter((item) =>
+				keyLS === 'answers' ? item.id === id : item.author === id
+		  ).length
+		: 0;
 };
